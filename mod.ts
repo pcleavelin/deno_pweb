3a91ed0fd82a85cabb43e14279de393c161e1eb0
Patrick Cleavelin <patrick@uptrainsoftware.com>
2020-02-08 13:49:18 -0800

    separate logic

--- mod.ts
+++ mod.ts
@@ -21,14 +21,78 @@ class Router {
 		this.routes = [];
 	}
 
+	private extract_route_params(url: string): string[] {
+		let args_begin = url.split('<');
+		let args = [];
+
+		args_begin.forEach(arg => {
+			let end_index = arg.indexOf('>');
+
+			let str = arg.substring(0, end_index);
+
+			if (str !== "") {
+				args.push(str);
+			}
+		})
+
+		return args;
+	}
+
+	private validate_and_parse_route_args(url: string, route_url: string): string[] {
+		let arg_values = [];
+		let req_index = 0;
+		let route_index = -1;
+
+		for (let i = 0; i < url.length; i++) {
+			route_index += 1;
+			if (url[i] === route_url[route_index]) {
+				continue;
+			}
+			else if (url[i] !== route_url[route_index]) {
+				if (route_url[route_index] === '<') {
+					let end_arg_index = route_url.indexOf('>', route_index);
+
+					let end_url_index = url.indexOf('/', i);
+					if (end_url_index === -1) {
+						end_url_index = url.length;
+					}
+
+					arg_values.push(url.substring(i, end_url_index));
+
+					route_index = end_arg_index+1;
+					i = end_url_index;
+				} else {
+					return null;
+				}
+			}
+		}
+
+		return arg_values;
+	}
+
+	private validate_route_args(route: WebRoute, route_args: string[]) {
+		let handler_str = route.handler.toString();
+		handler_str = handler_str.slice(handler_str.indexOf('(')+1, handler_str.indexOf(')'));
+
+		let params = handler_str.split(',').slice(1).map(param => param.trim());
+
+		if (params.length !== route_args.length) {
+			throw new Error(`mismatched route parameters: Found ${params.length} parameters, expected ${route_args.length}`);
+		}
+
+		route_args.forEach((arg, index) => {
+			if(arg !== params[index]) {
+				throw new Error(`mismatched route parameter: Found '${params[index]}', expected '${arg}'`);
+			}
+		});
+	}
+
 	public add_route(url: string, handler: Function) {
 		this.routes.push(new WebRoute(url, handler));
 	}
 
 	public async route_request(req: ServerRequest) {
-		// console.log(req);
-
-		console.log(`\n\nUrl: ${req.url}`);
+		console.log(`Url: ${req.url}`);
 
 		let url = req.url;
 		if (url[url.length - 1] === '/') {
@@ -36,91 +100,24 @@ class Router {
 		}
 
 		let num_paths = url.split('/').length - 1;
-		let arg_patt = /\<[a-zA-Z]+\>/;
-
-		console.log(`Number of paths: ${num_paths}`)
-
 		let final_args = [];
 		let final_route_args = [];
 
 		let route = this.routes.find(route => {
-			let args_begin = route.url.split('<');
-
-			console.log(`Raw args: ${args_begin}`);
-
-			let args = [];
-
-			args_begin.forEach(arg => {
-				let end_index = arg.indexOf('>');
-
-				let str = arg.substring(0, end_index);
-
-				if (str !== "") {
-					args.push(str);
-				}
-			})
-
-			final_route_args = args;
-
-			console.log(`Args: ${JSON.stringify(args)}`);
+			final_route_args = this.extract_route_params(route.url);
 
 			if ((route.url.split('/').length-1) !== num_paths) {
 				return false;
 			} 
 			else {
-				console.log("Same number of paths!");
+				final_args = this.validate_and_parse_route_args(url, route.url);
 
-				let arg_values = [];
-
-				let req_index = 0;
-				let route_index = -1;
-
-				for (let i = 0; i < url.length; i++) {
-					route_index += 1;
-					if (url[i] === route.url[route_index]) {
-						continue;
-					}
-					else if (url[i] !== route.url[route_index]) {
-						if (route.url[route_index] === '<') {
-							let end_arg_index = route.url.indexOf('>', route_index);
-
-							let end_url_index = url.indexOf('/', i);
-							if (end_url_index === -1) {
-								end_url_index = url.length;
-							}
-
-							arg_values.push(url.substring(i, end_url_index));
-							console.log(`Got arg: ${arg_values[arg_values.length-1]}`);
-
-							route_index = end_arg_index+1;
-							i = end_url_index;
-						} else {
-							return false;
-						}
-					}
-				}
-
-				final_args = arg_values;
-
-				return true;
+				return final_args === null ? false : final_args;
 			}
 		});
 
 		if (route !== undefined) {
-			let handler_str = route.handler.toString();
-			handler_str = handler_str.slice(handler_str.indexOf('(')+1, handler_str.indexOf(')'));
-
-			let params = handler_str.split(',').slice(1).map(param => param.trim());
-
-			if (params.length !== final_route_args.length) {
-				throw new Error(`mismatched route parameters: Found ${params.length} parameters, expected ${final_route_args.length}`);
-			}
-
-			final_route_args.forEach((arg, index) => {
-				if(arg !== params[index]) {
-					throw new Error(`mismatched route parameter: Found '${params[index]}', expected '${arg}'`);
-				}
-			});
+			this.validate_route_args(route, final_route_args);
 
 			try {
 				route.handler(req, ...final_args);
